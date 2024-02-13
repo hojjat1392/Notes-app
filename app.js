20fefece1f1dd89cc784c4926a458a337bd8f7de
hojjatAllah1371 <hojjat.rezapoor95@gmail.com>
2024-02-13 21:30:01 +0330

    Add Yargs module and some code is wrote

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-const validator = require('validator');
-const chalk = require('chalk');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
+// Customize yargs version
+yargs.version('1.1.0')
 
-const getNotes = require('./notes');
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
 
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
 
-const note = getNotes('Hi my name is hojjat')
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
 
-console.log(chalk.red.inverse("Error!"));
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
 
- 
+console.log(yargs.argv)
