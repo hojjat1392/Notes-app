d15d41d24275693321ec78585c2219fc6cb1f061
hojjatAllah1371 <hojjat.rezapoor95@gmail.com>
2024-02-19 19:59:39 +0330

    Working on add command 2

--- notes.js
+++ notes.js
@@ -5,25 +5,31 @@ const yargs = require("yargs");
 //load notes
 const loadNotes = () => {
   try {
-    const dataBuffer = fs.readFileSync("data.json");
+    const dataBuffer = fs.readFileSync("notes.json");
     const dataJson = dataBuffer.toString();
     console.log(chalk.blue("File loaded successfully!"));
     return JSON.parse(dataJson);
   } catch (e) {
-    console.log(chalk.red("Something is wrong!"));
+    return [];
   }
 };
 
 //add notes function
 const addNote = (title, body) => {
-  const notes = loadNotes();
+  notes = loadNotes();
 
-  notes.title = title;
-  notes.body = body;
+  notes.push({
+    title: title,
+    body: body,
+  });
 
+  saveNotes(notes);
+};
+
+const saveNotes = function (notes) {
   const writeNote = JSON.stringify(notes);
   try {
-    fs.appendFileSync("data.json", writeNote);
+    fs.writeFileSync("notes.json", writeNote);
     console.log(chalk.green("New task was added successfully!"));
   } catch (e) {
     console.log(chalk.red("Problem to adding tasks!. Something is wrong!"));
